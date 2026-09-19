2eee4354542bfd7927b210333c999cdf4bf09af5
whoareu-btw <orangganteng87659@gmail.com>
2026-09-19 12:29:36 +0700

    Add configuration screencast for xdg-desktop-portal-wlr at rikka/env/flatpak/flatpak.nix

--- rikka/env/flatpak/flatpak.nix
+++ rikka/env/flatpak/flatpak.nix
@@ -2,11 +2,24 @@
 
 {
   services.flatpak.enable = true;
+
   xdg.portal = {
     enable = true;
-    wlr.enable = true;
+    wlr = {
+      enable = true;
+      settings = {
+        screencast = {
+          chooser_type = "simple";
+          chooser_cmd = "${pkgs.slurp}/bin/slurp -f %o -or";
+        };
+      };
+    };
+
     extraPortals = with pkgs; [
+      xdg-desktop-portal-wlr
       xdg-desktop-portal-gtk
     ];
+
+    config.common.default = [ "gtk" "wlr" ];
   };
 }
